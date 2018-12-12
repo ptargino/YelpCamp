6b840d4c83571f65e920d2b229c5f6f1685246e6
Philippe Targino <ptargino@gmail.com>
2018-12-12 14:16:16 +0000

    Added comments

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    Campground = require("./models/campgrounds"),
+    Comment = require("./models/comments"),
+    seedDB = require("./seeds");
+    
+seedDB();
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
-//Schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -31,18 +27,22 @@ app.get("/campgrounds", function(req, res) {
     })
 });
 
-app.post("/campgrounds", function(req, res) {
-    Campground.create({ name: req.body.name, image: req.body.image, description: req.body.description }, function(err, campground) {
-        if (err) {
+//CREATE - add new campground to DB
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
             console.log(err);
-        }
-        else {
-            console.log("Campground created");
-            console.log(campground);
+        } else {
+            //redirect back to campgrounds page
             res.redirect("/campgrounds");
         }
     });
-
 });
 
 app.get("/campgrounds/new", function(req, res) {
